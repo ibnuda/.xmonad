7e258ad266108bb9d1760a12adcbd753c8cd4ef0
Ibnu D. Aji <teratower8@gmail.com>
2017-08-14 23:48:47 +0700

    well, back to 1366.

--- autostart.sh
+++ autostart.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 xrdb ~/.Xresources
 #feh --bg-fill /home/$USER/.xmonad/bg/awan.jpg &
-feh --bg-fill /home/mel/Pictures/background/chloe.jpg &
+#feh --bg-fill /home/mel/Pictures/background/chloe.jpg &
 xsetroot -cursor_name left_ptr &
-udiskie &
 urxvtd &
 start-pulseaudio-x11 --log-target=syslog &
 sleep 10; amixer -c 1 set Headphone 100% unmute 
--- xmonad.hs
+++ xmonad.hs
@@ -66,7 +66,7 @@ myXPConfig =
                     , bgHLight  = "#FFFFFF"
                     , fgHLight  = "#000000"
                     , promptBorderWidth = 0
-                    , height    = 50
+                    , height    = 20
                     }
 
 -- myManageHook :: Query (Data.Monoid.Endo WindowSet)
@@ -92,49 +92,49 @@ myDzenPP = dzenPP
     }
 
 dzenMenu :: [Char]
-dzenMenu        = "(zsh ~/.xmonad/menu.sh) | (dzen2 -w '100' -y '1150' " ++ konfigurasiDzen ++ eksekusiMenu
+dzenMenu        = "(zsh ~/.xmonad/menu.sh) | (dzen2 -w '30' -y '748' " ++ konfigurasiDzen ++ eksekusiMenu
 
 eksekusiMenu :: [Char]
 eksekusiMenu    = " -m -p -l 7  -e 'button3=togglecollapse;leaveslave=collapse;button1=menuexec')"
 
 dzenTitleBar :: [Char]
-dzenTitleBar    = "dzen2 -x '100' -y '1150' -w '1300' "++ konfigurasiDzen
+dzenTitleBar    = "dzen2 -x '30' -y '748' -w '1068' "++ konfigurasiDzen
 
 dzenConky :: [Char]
-dzenConky       = "conky -c ~/.xmonad/conkyrc | dzen2 -x '1400' -y '1150' -w '520' " ++ konfigurasiDzen
+dzenConky       = "conky -c ~/.xmonad/conkyrc | dzen2 -x '1068' -y '748' -w '300' " ++ konfigurasiDzen
 
 konfigurasiDzen :: [Char]
-konfigurasiDzen = "-ta 'l' -h '50' -fg '#9f9fa2' -bg '#2b2b28' -fn '" ++ fontDzen ++ "'"
+konfigurasiDzen = "-ta 'l' -h '20' -fg '#9f9fa2' -bg '#2b2b28' -fn '" ++ fontDzen ++ "'"
 
 fontDzen :: [Char]
-fontDzen        = "-*-source code pro-medium-r-normal-*-24-*-*-*-*-*-*-*"
+fontDzen        = "-*-fira code-medium-r-normal-*-12-*-*-*-*-*-*-*"
 
 myKeys conf@(XConfig {XMonad.modMask = mod}) = M.fromList $
     [ ((mod .|. shiftMask       , xK_Return     ), spawn $ XMonad.terminal conf)
     , ((mod .|. shiftMask       , xK_q          ), spawn "killall udiskie ; killall dzen2; killall compton; killall urxvtd" >> io (exitWith ExitSuccess))
-    , ((mod .|. shiftMask       , xK_o          ), safePrompt "google-chrome-stable" myXPConfig)
-    , ((mod .|. shiftMask       , xK_p          ), shellPrompt myXPConfig)
-    , ((mod .|. shiftMask       , xK_n          ), raiseMaybe (runInTerm "-title lagu" "ncmpcpp") (title =? "ncmpcpp"))
-    , ((mod .|. shiftMask       , xK_x          ), kill)
+    , ((mod .|. shiftMask       , xK_r          ), safePrompt "google-chrome-stable" myXPConfig)
+    , ((mod .|. shiftMask       , xK_l          ), shellPrompt myXPConfig)
+    , ((mod .|. shiftMask       , xK_b          ), raiseMaybe (runInTerm "-title lagu" "ncmpcpp") (title =? "ncmpcpp"))
+    , ((mod .|. shiftMask       , xK_k          ), kill)
     , ((mod .|. shiftMask       , xK_space      ), setLayout $ XMonad.layoutHook conf)
-    , ((mod .|. shiftMask       , xK_s          ), spawn "xfce4-screenshooter")
+    , ((mod .|. shiftMask       , xK_o          ), spawn "xfce4-screenshooter")
     , ((mod .|. shiftMask       , xK_Right      ), shiftToNext)
-    , ((mod .|. shiftMask       , xK_j          ), nextWS)
+    , ((mod .|. shiftMask       , xK_h          ), nextWS)
     , ((mod .|. shiftMask       , xK_Left       ), shiftToPrev)
-    , ((mod .|. shiftMask       , xK_k          ), prevWS)
+    , ((mod .|. shiftMask       , xK_t          ), prevWS)
     , ((mod .|. shiftMask       , xK_a          ), spawn $ XMonad.terminal conf)
     , ((mod .|. shiftMask       , xK_Tab        ), windows W.focusUp)
     , ((mod .|. controlMask     , xK_Right      ), shiftToNext >> nextWS)
     , ((mod .|. controlMask     , xK_Left       ), shiftToPrev >> prevWS)
-    , ((mod                     , xK_q          ), spawn "if type xmonad; then killall dzen2 && xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\";fi")
+    , ((mod                     , xK_p          ), spawn "if type xmonad; then killall dzen2 && xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\";fi")
     , ((mod                     , xK_Tab        ), windows W.focusDown)
     , ((mod                     , xK_comma      ), sendMessage (IncMasterN 1))
     , ((mod                     , xK_period     ), sendMessage (IncMasterN (-1)))
-    , ((mod                     , xK_t          ), withFocused $ windows . W.sink)
-    , ((mod                     , xK_l          ), sendMessage Expand)
-    , ((mod                     , xK_j          ), windows W.focusDown)
-    , ((mod                     , xK_k          ), windows W.focusUp)
-    , ((mod                     , xK_h          ), sendMessage Shrink)
+    , ((mod                     , xK_y          ), withFocused $ windows . W.sink)
+    , ((mod                     , xK_n          ), sendMessage Expand)
+    , ((mod                     , xK_h          ), windows W.focusDown)
+    , ((mod                     , xK_t          ), windows W.focusUp)
+    , ((mod                     , xK_d          ), sendMessage Shrink)
     , ((mod                     , xK_Return     ), windows W.swapMaster)
     , ((mod                     , xK_m          ), windows W.swapDown)
     , ((mod                     , xK_F1         ), manPrompt            myXPConfig)
@@ -149,7 +149,7 @@ myKeys conf@(XConfig {XMonad.modMask = mod}) = M.fromList $
     , ((mod                     , xK_Next       ), nextWS)
     , ((mod                     , xK_Prior      ), prevWS)
     , ((mod                     , xK_z          ), spawn "thunar")
-    , ((mod                     , xK_o          ), spawn "google-chrome-stable")
+    , ((mod                     , xK_o          ), spawn "firefox")
     , ((mod                     , xK_space      ), sendMessage NextLayout)
     ]
     ++
